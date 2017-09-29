3435b8938cc018e63e2f8aa45dbfb41cf1c7a1ce
prem <pkumar16@jcp.com>
2017-09-29 01:56:36 -0500

    updated bat file

--- EditDNS.bat
+++ EditDNS.bat
@@ -1,4 +1,4 @@
 @echo off
 (echo 23.41.208.114 m.jcpenney.com
 echo 23.41.208.114 www.jcpenney.com) > "C:\temp\temp.txt"
-copy /Y "C:\temp\temp.txt" C:\WINDOWS\system32\drivers\etc\hosts
+copy /Y "C:\temp\temp.txt" %WINDIR%\system32\drivers\etc\hosts
