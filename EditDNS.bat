78bd968314bf9325b72f0d5037498e822c28406f
prem <pkumar16@jcp.com>
2017-10-03 14:45:25 -0500

    updated file

--- EditDNS.bat
+++ EditDNS.bat
@@ -1,5 +1,6 @@
 @echo off
-(echo 23.41.208.114 m.jcpenney.com
-echo 23.41.208.114 www.jcpenney.com
+(echo 72.247.152.138 www.jcpenney.com
+echo 72.247.152.138 account-api.jcpenney.com
+echo 72.247.152.138 m.jcpenney.com
 echo 162.222.75.243 www.google.com) > %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
