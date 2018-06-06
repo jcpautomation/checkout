8925657fac0b03064bf8aa74b68207f99d9726ed
prem <pkumar16@jcp.com>
2018-06-06 14:09:32 -0500

    updated ip

--- EditDNS.bat
+++ EditDNS.bat
@@ -1,10 +1,9 @@
 @echo off
-(echo 23.41.208.114 www.jcpenney.com
-echo 23.41.208.114 m.jcpenney.com
-echo 23.41.208.114 order-api.jcpenney.com
-echo 23.41.208.114 account-api.jcpenney.com
-echo 23.41.208.114 content-api.jcpenney.com
-echo 23.41.208.114 search-api.jcpenney.com
-echo 23.41.208.114 browse-api.jcpenney.com
-echo 72.247.152.138 m.jcpenney.com) > %temp%\temphosts.txt
+(echo 23.201.16.116 www.jcpenney.com
+echo 23.201.16.116 m.jcpenney.com
+echo 23.201.16.116 order-api.jcpenney.com
+echo 23.201.16.116 account-api.jcpenney.com
+echo 23.201.16.116 content-api.jcpenney.com
+echo 23.201.16.116 search-api.jcpenney.com
+echo 23.201.16.116 browse-api.jcpenney.com) > %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
